f5ca5cb2b6e442ffded894ae97e139bfcdc3ca7f
maya1795 <mayasameh18@gmail.com>
2023-09-08 17:34:17 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,19 +9,14 @@
 
 int main(void)
 {
-	int n = 0;
-	char ch = 'a';
+	int n = 48;
 
-	while (n <= 17)
+	while (n <= 102)
 	{
-		putchar(n + '0');
+		putchar(n);
+		if (n == 57)
+			n += 39;
 		n++;
-	}
-
-	while (ch <= 'f')
-	{
-		putchar(ch);
-		ch++;
 	}
 	putchar('\n');
 	return (0);
