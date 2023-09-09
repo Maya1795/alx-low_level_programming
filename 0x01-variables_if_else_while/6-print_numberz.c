7d9faaaadc8a966b0e23dc057b83744697bd1f5a
maya1795 <mayasameh18@gmail.com>
2023-09-08 17:15:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
