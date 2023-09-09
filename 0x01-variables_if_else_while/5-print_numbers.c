53439cde7ec5c5616c18aca74c02ac5d0d528179
maya1795 <mayasameh18@gmail.com>
2023-09-08 17:11:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
+
