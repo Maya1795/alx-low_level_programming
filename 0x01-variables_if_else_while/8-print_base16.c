2984b712f750b7b1dd94721daf3fd5243f3196f3
maya1795 <mayasameh18@gmail.com>
2023-09-08 17:28:18 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+	char ch = 'a';
+
+	while (n <= 17)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	while (ch <= f)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
