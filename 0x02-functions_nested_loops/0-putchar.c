45c6e694cebe09845814048efd00ed915f7eb5be
maya1795 <mayasameh18@gmail.com>
2023-09-12 15:49:29 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char m[]  = "_putchar";
+
+	for (int i=0; ch < 8; i++)
+	{
+		_putchar(m[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
