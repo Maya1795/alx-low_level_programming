6f3a09c3a3957bd489779c0779ece69336ee0d56
maya1795 <mayasameh18@gmail.com>
2023-09-14 18:53:13 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,11 @@
 
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	do {
 		_putchar(i + 48);
-	_putchar("\n");
+		i++;
+	} while (i >= 0 && i <= 9);
+	_putchar('\n');
 }
