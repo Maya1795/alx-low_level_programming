b88fa18f831af6dfb3ad55bc83e9e97f0c4d116e
maya1795 <mayasameh18@gmail.com>
2023-09-21 11:25:18 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - determine accis is lower
+ * @c: para
+ * Return: 1
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isdelimiter - dete delimiter
+ *
+ * @c: chara
+ *
+ * Return: 1
+ *
+ */
+
+int isdelimiter(char c)
+{
+	int i;
+	char d[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == d[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalizer all word od string
+ *
+ * @s: input string
+ *
+ * Return: string with capitlization words
+ *
+ */
+
+char *cap_string(char *s)
+{
+	int *p = s;
+	int f = 1;
+
+	while (*s)
+	{
+		if (isdelimiter(*s))
+			f = 1;
+		else if (isLower(*s) && f)
+		{
+			*s -= 32;
+			f = 0;
+		}
+		else
+			f = 0;
+		s++;
+	}
+	return (p);
+}
