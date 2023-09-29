6e5eb68a0d5f8e988c44ceb08b304f4da6b9b2ee
maya1795 <mayasameh18@gmail.com>
2023-09-28 17:29:09 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root
+ *
+ * @n: number
+ *
+ * Return: square root of number
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sq(n, 1));
+}
+
+/**
+ * sq - square
+ *
+ * @n: number
+ * @m: value
+ *
+ * Return: number
+ *
+ */
+int sq(int n, int m)
+{
+	if (m * m == n)
+		return (m);
+	else if (m * m < n)
+		return (sq(n, m + 1));
+	else
+		return (-1);
+}
