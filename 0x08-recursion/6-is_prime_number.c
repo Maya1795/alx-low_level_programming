a6233914265821342eaa8291821740ef2cd587d9
maya1795 <mayasameh18@gmail.com>
2023-09-28 17:53:39 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,4 +2,34 @@
 #include <stdio.h>
 
 /**
- * */
+ * cp - check prime
+ *
+ * @n: number
+ * @m:check
+ *
+ * Return:check
+ *
+ */
+int cp(int n, int m)
+{
+	if (m >= n && n >= 2)
+		return (1);
+	else if (n % m == 0 || n <= 1)
+		return (0);
+	else
+		return (cp(n, m + 1));
+}
+
+/**
+ * is_prime_number - check if no. is prime
+ *
+ * @n: number
+ *
+ * Return: check
+ *
+ */
+
+int is_prime_number(int n)
+{
+	return (cp(n, 2));
+}
