1cd9b8b697375d4b23c8968461f66c6f89166cfa
maya1795 <mayasameh18@gmail.com>
2023-09-28 19:07:44 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+int in(char *s1, char *s2);
+/**
+ * mp - move until reach the after star
+ *
+ * @s2: string
+ *
+ * Return: after star
+ *
+ */
+
+int mp(char *s2)
+{
+	if (*s2 == '*')
+		return (mp(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * wildcmp - compare 2 string
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int m = 0;
+
+	if (!*s1 && *s2 == '*' && !mp(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !*s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = mp(s2);
+		if (!*s2)
+			return (0);
+		if (*s1 == *s2)
+			m += wildcmp(s1 + 1, s2 + 1);
+		m += in(s1, s2);
+		return (!m);
+	}
+	return (0);
+}
+
+/**
+ * in - inception
+ *
+ * @s1: first string
+ * @s2: secong string
+ *
+ * Return: in
+ *
+ */
+int in(char *s1, char *s2)
+{
+	int m = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		m += wildcmp(s1 + 1, s2 + 1);
+	m += in(s1 + 1, s2);
+	return (m);
+}
