56cfd827e44daced2e013ad081d389214660b9ce
maya1795 <mayasameh18@gmail.com>
2023-10-04 16:07:17 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to ner all. space
+ *
+ * @str: string
+ *
+ * Return: pointer
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, s = 0;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[s] != '\0'; s++)
+		p = malloc(s * sizeof(*str) + 1);
+	if (p == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < s; i++)
+			p[i] = str[i];
+	}
+	return (p);
+}
