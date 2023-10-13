1999bafae586b7c2250fe1201cdf82f03050c863
maya1795 <mayasameh18@gmail.com>
2023-10-13 14:07:30 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,19 +23,19 @@ void print_all(const char * const format, ...)
 			switch (format[c])
 			{
 				case 'c':
-					printf("%s%c", s, va_arg(p, int));
+					printf("%s%c ", s, va_arg(p, int));
 					break;
 				case 'i':
-					printf("%s%d", s, va_arg(p, int));
+					printf("%s%d ", s, va_arg(p, int));
 					break;
 				case 'f':
-					printf("%s%f", s, va_arg(p, double));
+					printf("%s%f ", s, va_arg(p, double));
 					break;
 				case 's':
 					ptr = va_arg(p, char *);
 					if (ptr == 0)
 						ptr = "(nil)";
-					printf("%s%s", s, ptr);
+					printf("%s%s ", s, ptr);
 					break;
 				default:
 					c++;
