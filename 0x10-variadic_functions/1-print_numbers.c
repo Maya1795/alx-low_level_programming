4c499ababa28082d0ab76746f11dbb0209a336ed
maya1795 <mayasameh18@gmail.com>
2023-10-12 18:08:23 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print no.
+ *
+ * @separator: string
+ * @n: count
+ *
+ * Return: numbers
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int c = n;
+	va_list p;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(p, n);
+	while (c--)
+		printf("%d%s", va_arg(p, int),
+				i ? (separator ? separator : "") : "\n");
+	va_end(p);
+}
