2fb3d23f8a4eb1622fa769919cbe537d81e8c90f
Maya1795 <mayasameh18@gmail.com>
2023-10-19 09:53:17 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * len - length of string
+ *
+ * @m: string
+ *
+ * Return: length
+ *
+ */
+
+int len(char *m)
+{
+	int i = 0;
+
+	if (!m)
+		return (0);
+	while (*m++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - print list of string
+ *
+ * @h: pointer
+ *
+ * Return: number
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", len((h).str), (h).str ? (h).str : "(nil)");
+		h = (h).next;
+		j++;
+	}
+	return (j);
+}
