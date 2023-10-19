fbb56e85e3ceca6ca8a6a4858d4861550d551f2a
Maya1795 <mayasameh18@gmail.com>
2023-10-19 10:53:57 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - free list
+ *
+ * @head: pointer
+ *
+ * Return: number
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *p, *m;
+
+	if (head == 0)
+		return (0);
+	m = head;
+	while (m)
+	{
+		p = m->next;
+		free(m->str);
+		free(m);
+		m = p;
+	}
+}
