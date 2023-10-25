b6e244fe5be7b14e786a36cbc6468b5ce30476fc
Maya1795 <mayasameh18@gmail.com>
2023-10-25 16:15:47 -0700

    besm allah

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-
+listint_t *f(listint_t *head);
 /**
  * free_listint_safe - free list
  *
@@ -12,31 +12,55 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t c = 0;
-	int m;
-	listint_t *p;
+	int m = 1;
+	listint_t *p, *l;
 
 	if (h == NULL || *h == 0)
 		return (0);
-	while (*h)
+	l = f(*h);
+	for (c = 0; (*h != l || m) && *h != NULL; *h = next)
 	{
-		m = *h - (*h)->next;
-		if (m > 0)
+		c++;
+		p = (*h)->next;
+		if (*h == l && m)
 		{
-			p = (*h)->next;
-			free(*h);
-			*h = p;
+			if (l == l->next)
+			{
+				free(*h);
+				break;
+			}
 			c++;
+			p = p->next;
+			free((*h)->next);
+			m = 0;
 		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			c++;
-			break;
-		}
+		free(*h);
 	}
-	*h = NULL;
-	return (c);
 }
 
+/**
+ * f - find loop
+ *
+ * @h: pointer
+ *
+ * Retur: position
+ *
+ */
+listint_t *f(listint_t *head)
+{
+	listint_t *p, *e;
 
+	if (head == NULL)
+		return (0);
+	for (e = head->next; e != NULL; e = e->next)
+	{
+		if (e == e->next)
+			return (e);
+		for (p = head; p != e; p = p->next)
+		{
+			if (p == e->next)
+				return (e->next);
+		}
+	}
+	return (NULL);
+}
