04f2aee9c8a1ff2aa8a64c79d8c41ef52cb437c4
Maya1795 <mayasameh18@gmail.com>
2023-10-26 16:29:14 -0700

    yarab

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,9 +12,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int m = (*n != 1l << index);
-
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	return (!!(m));
+	return (!!(*n != 1l << index));
 }
